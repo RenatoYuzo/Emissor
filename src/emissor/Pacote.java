10321ca7ef861aafe01197f53895b97e643cbcc1
RenatoYuzo <renatoyuzo93@gmail.com>
2018-06-24 01:02:27 -0400

    Removendo alguns prinf's

--- src/emissor/Pacote.java
+++ src/emissor/Pacote.java
@@ -1,41 +1,27 @@
 package emissor;
 
 public class Pacote {
-	
-	private String ipOrigem;
-	private String ipDestino;
-	private String mensagem;
-	private Integer TTL;
-	
-	public Pacote(String ipOrigem, String ipDestino, String msg) {
-		this.ipOrigem = ipOrigem;
-		this.ipDestino = ipDestino;
-		this.mensagem = msg;
-		this.TTL = 6;
-	}
 
-	public String getIpOrigem() {
-		return this.ipOrigem;
-	}
+    private String ipOrigem;
+    private String ipDestino;
+    private String mensagem;
+    private Integer TTL;
 
-	public String getIpDestino() {
-		return this.ipDestino;
-	}
+    public Pacote(String ipOrigem, String ipDestino, String msg) {
+        this.ipOrigem = ipOrigem;
+        this.ipDestino = ipDestino;
+        this.mensagem = msg;
+        this.TTL = 6;
+    }
 
-	public String getMensagem() {
-		return this.mensagem;
-	}
+    public String getIpOrigem() { return this.ipOrigem; }
+    public String getIpDestino() { return this.ipDestino; }
+    public String getMensagem() { return this.mensagem; }
+    public Integer getTTL() { return this.TTL; }
 
-	public Integer getTTL() {
-		return this.TTL;
-	}
-
-	@Override
-	public String toString() {
-		return 	this.ipOrigem + "," +
-				this.ipDestino + "," +
-				this.mensagem + "," +
-				this.TTL;
-	}
+    @Override
+    public String toString() {
+        return this.ipOrigem+","+this.ipDestino+","+this.mensagem + ","+ this.TTL;
+    }
 
 }
